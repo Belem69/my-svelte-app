a3bcfee58fe3938f3e7b43f2f6fd6a69f64fe7ca
bibisixtynine <85257463+bibisixtynine@users.noreply.github.com>
2022-12-30 02:36:09 +0100

    componet box added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Box from '../lib/Box.svelte';
+</script>
+
+<Box>
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+        Visite <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+</Box>
+
+<style>
+    h1 {
+        background-color: #cc1e1e;
+        text-align: center;
+    }
+</style>
