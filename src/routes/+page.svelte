a2ad4d3f0e3015efd736e09acb30378064bc7c52
bibisixtynine <85257463+bibisixtynine@users.noreply.github.com>
2022-12-31 01:08:11 +0100

    added svelte CM6, Clock ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,41 @@
 <script>
-    import Box from '../lib/Box.svelte';
+    import Box from "$lib/Box.svelte";
+    import Clock from "$lib/Clock.svelte";
+    import Editor from "$lib/Editor.svelte";
 </script>
 
-<Box>
-    <h1>Welcome to SvelteKit</h1>
-    <p>
-        Visite <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-    </p>
-</Box>
+<div class="container">
+    <Box>
+        <h1>Welcome to SvelteKit</h1>
+        <p>
+            Visite <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+            documentation
+        </p>
+    </Box>
+
+    <Box>
+        <Editor />
+    </Box>
+
+    <div class="clock">
+        <Clock /><Clock />
+    </div>
+</div>
 
 <style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
     h1 {
         background-color: #cc1e1e;
         text-align: center;
+    }
+
+    .clock {
+        display: flex;
+        width: 50%;
     }
 </style>
